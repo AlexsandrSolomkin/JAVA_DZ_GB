a438d31eee6cee5cadd75bcac1ab8fc89bbe5f57
Solomkin Alexandr <solomin1995@mail.ru>
2023-02-14 16:23:56 +0300

    DZ_1_2.java: creating a method that counts the number of primes from 1 to n (version_2)

--- DZ_JAVA/DZ_SEMINAR_1/DZ_1_2.java
+++ DZ_JAVA/DZ_SEMINAR_1/DZ_1_2.java
@@ -18,32 +18,39 @@ public class DZ_1_2 {
     static int count_arr_prime_num_1_n(int end_n) {
         int count = 0;
 
-        if (end_n == 1) {
-            return count = 1;
-        } else if (end_n == 2) {
-            return count = 2;
-        } else if ((end_n > 2) && (end_n > 0)) {
-            count = 2;
-
-            for (int j = 3; j <= end_n; j++) {
-                for (int i = 2; i <= j; i++) {
-                    if (i == j) {
-                        count++;
-                    } else if (j % i == 0) {
-                        break;
-                    }
+        for (int j = 1; j <= end_n; j++) {
+            for (int i = 1; i <= j; i++) {
+                if (i == j) {
+                    count++;
+                } else if ((j % i == 0) && (i > 1)) {
+                    break;
                 }
             }
-            return count;
         }
         return count;
+        
     }
 
     // static int[] arr_prime_num_1_n(int count_n) {
     //     int[] arr = new int[count_n];
 
-    //     for (int i = 1; i <= end_n; i++) {
-    //         arr.append(i);
+    //     if (count_n == 1) {
+    //         return arr = new int[] {1};
+    //     } else if (count_n == 2) {
+    //         return arr = new int[] {1, 2};
+    //     } else if ((count_n > 2) && (count_n > 0)) {
+    //         int count_arr = 2;
+
+    //         for (int j = 3; j <= count_n; j++) {
+    //             for (int i = 2; i <= j; i++) {
+    //                 if (i == j) {
+    //                     count++;
+    //                 } else if (j % i == 0) {
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //         return count;
     //     }
 
     //     return arr;
