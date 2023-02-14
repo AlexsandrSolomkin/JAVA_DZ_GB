fd3cc83efdf15e9441cf1dec31f7127f85418c48
Solomkin Alexandr <solomin1995@mail.ru>
2023-02-14 17:12:12 +0300

    DZ_1_2.java: creating a method that outputs an array of elements consisting of prime numbers from 1 to n

--- DZ_JAVA/DZ_SEMINAR_1/DZ_1_2.java
+++ DZ_JAVA/DZ_SEMINAR_1/DZ_1_2.java
@@ -9,10 +9,18 @@ package DZ_JAVA.DZ_SEMINAR_1;
 public class DZ_1_2 {
 
     public static void main(String[] args) {
-        int num = 1000;
+        int num = 20;
+        int ell_arr = count_arr_prime_num_1_n(num);
+        int[] arr_ell = arr_prime_num_1_n(ell_arr, num);
 
-        System.out.printf("Простых чисел от 1 до n: %d\n", count_arr_prime_num_1_n(num));
+        System.out.printf("n = %d\n", num);
+        System.out.printf("Простых чисел от 1 до n: %d\n", ell_arr);
+        System.out.printf("Простые числа от 1 до n:\n");
 
+        for (int item : arr_ell) {
+            System.out.printf("%d\n", item);
+        }
+        
     }
 
     static int count_arr_prime_num_1_n(int end_n) {
@@ -31,31 +39,22 @@ public class DZ_1_2 {
         
     }
 
-    // static int[] arr_prime_num_1_n(int count_n) {
-    //     int[] arr = new int[count_n];
+    static int[] arr_prime_num_1_n(int count_n, int end_n) {
+        int[] arr = new int[count_n];
+        int index = 0;
 
-    //     if (count_n == 1) {
-    //         return arr = new int[] {1};
-    //     } else if (count_n == 2) {
-    //         return arr = new int[] {1, 2};
-    //     } else if ((count_n > 2) && (count_n > 0)) {
-    //         int count_arr = 2;
-
-    //         for (int j = 3; j <= count_n; j++) {
-    //             for (int i = 2; i <= j; i++) {
-    //                 if (i == j) {
-    //                     count++;
-    //                 } else if (j % i == 0) {
-    //                     break;
-    //                 }
-    //             }
-    //         }
-    //         return count;
-    //     }
-
-    //     return arr;
-    // }
-
+        for (int k = 1; k <= end_n; k++) {
+            for (int l = 1; l <= k; l++) {
+                if (l == k) {
+                    arr[index] = k;
+                    index++;
+                } else if ((k % l == 0) && (l > 1)) {
+                    break;
+                }
+            }
+        }
+        return arr;
+    }
 }
 
 // ============================================================================
