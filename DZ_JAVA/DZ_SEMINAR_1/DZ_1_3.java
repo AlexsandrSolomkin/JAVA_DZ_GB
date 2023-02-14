42ffd0f8fc6f0da3ff416a73d05b28c9f20f4110
Solomkin Alexandr <solomin1995@mail.ru>
2023-02-14 18:26:40 +0300

    DZ_1_2.java: block with data input and response output

--- DZ_JAVA/DZ_SEMINAR_1/DZ_1_3.java
+++ DZ_JAVA/DZ_SEMINAR_1/DZ_1_3.java
@@ -6,7 +6,23 @@
 
 package DZ_JAVA.DZ_SEMINAR_1;
 
+import java.util.Scanner;
+
 public class DZ_1_3 {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите число: ");
+        double num_1 = iScanner.nextDouble();
+        System.out.printf("Введите действие(+, -, *, /): ");
+        String action = iScanner.nextLine();
+        System.out.printf("Введите число: ");
+        double num_2 = iScanner.nextDouble();
+
+        double res = 0;
+
+        System.out.printf("%f %s %f = %f", num_1, action, num_2, res);
+    }
+
     
 }
 
