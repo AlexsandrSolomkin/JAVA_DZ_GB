699b5996f7ce1e20969162c3649f7d0517196c87
Solomkin Alexandr <solomin1995@mail.ru>
2023-03-01 00:21:58 +0300

    DZ_3_2.java: complete

--- DZ_JAVA/DZ_SEMINAR_3/DZ_3_2.java
+++ DZ_JAVA/DZ_SEMINAR_3/DZ_3_2.java
@@ -2,8 +2,50 @@
 
 package DZ_JAVA.DZ_SEMINAR_3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class DZ_3_2 {
     public static void main(String[] args) {
-        
+        List<Integer> n_arr = new ArrayList<>();
+        List<Integer> new_arr = new ArrayList<>();
+
+        n_arr = arr_rand(0, 100, 30);
+
+        for (int i : n_arr) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        new_arr = new_l(n_arr);
+
+        for (int i : new_arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    static List<Integer> arr_rand(int min_a, int max_a, int num_a) {
+        List<Integer> new_arr = new ArrayList<>();
+        Random rand = new Random();
+
+        for (int i = 0; i < num_a; i++) {
+            new_arr.add(rand.nextInt(min_a, max_a));
+        }
+
+        return new_arr;
+    }
+
+    static List<Integer> new_l(List<Integer> list_a){
+        for(int i = 0; i < list_a.size(); i++){
+            if (list_a.get(i) % 2 == 0){
+                list_a.remove(i);
+                i--;
+            }
+        }
+
+        return list_a;
     }
 }
