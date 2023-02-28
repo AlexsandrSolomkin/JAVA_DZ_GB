881f9d22820e10383439212d881b7bcdf0a6b979
Solomkin Alexandr <solomin1995@mail.ru>
2023-02-28 23:57:56 +0300

    create new file DZ_3_2.java

---
+++ DZ_JAVA/DZ_SEMINAR_3/DZ_3_2.java
@@ -0,0 +1,9 @@
+// 2 Пусть дан произвольный список целых чисел, удалить из него четные числа
+
+package DZ_JAVA.DZ_SEMINAR_3;
+
+public class DZ_3_2 {
+    public static void main(String[] args) {
+        
+    }
+}
