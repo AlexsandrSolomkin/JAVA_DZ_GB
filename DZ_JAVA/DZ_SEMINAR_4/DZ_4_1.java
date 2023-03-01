5b5caa84ce4b72ae1c23aaeedd885514315ad9f8
Solomkin Alexandr <solomin1995@mail.ru>
2023-03-01 22:20:03 +0300

    create new file DZ_4_1.java

---
+++ DZ_JAVA/DZ_SEMINAR_4/DZ_4_1.java
@@ -0,0 +1,12 @@
+// HashMap<Login, Список телефонов>
+// - добалять телефон
+// - удалять телефон
+// - находить по логину телефон/список телефонов
+
+package DZ_JAVA.DZ_SEMINAR_4;
+
+public class DZ_4_1 {
+    public static void main(String[] args) {
+        
+    }
+}
