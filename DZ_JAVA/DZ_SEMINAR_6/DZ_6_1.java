8dcc38e5f4b76710b412a09dfd7c46abae4cdf90
Solomkin Alexandr <solomin1995@mail.ru>
2023-03-10 00:15:16 +0300

    DZ_6_1.java: creating a TreeNode class

--- DZ_JAVA/DZ_SEMINAR_6/DZ_6_1.java
+++ DZ_JAVA/DZ_SEMINAR_6/DZ_6_1.java
@@ -1,7 +1,36 @@
+// Учитывая двоичного дерева, верните порядок обхода значений его узлов.
+
+// пример
+
+// Input: root = [1,null,2,3]
+// Output: [1,2,3]
+
 package DZ_JAVA.DZ_SEMINAR_6;
 
 public class DZ_6_1 {
     public static void main(String[] args) {
         
     }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
+
+
