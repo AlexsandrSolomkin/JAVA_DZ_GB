3d16d4d4a36f26440a12507a1322485b4895997d
Solomkin Alexandr <solomin1995@mail.ru>
2023-03-12 15:11:38 +0300

    DZ_6_1.java: complete

--- DZ_JAVA/DZ_SEMINAR_6/DZ_6_1.java
+++ DZ_JAVA/DZ_SEMINAR_6/DZ_6_1.java
@@ -7,30 +7,32 @@
 
 package DZ_JAVA.DZ_SEMINAR_6;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DZ_6_1 {
     public static void main(String[] args) {
-        
+        TreeNode tNode = new TreeNode(1, null, new TreeNode(2, null, new TreeNode(3)));
+        System.out.println(preorderTraversal(tNode));
     }
 
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-
+    public static List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root != null) {
+            list.add(root.val);
+            list.addAll(preorderTraversal(root.left));
+            list.addAll(preorderTraversal(root.right));
         }
+        return list;
+    }
 
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
+    public void fillTree(TreeNode root, Integer in) {
+        if (root == null) {
+            root.val = in;
+        } else if (root.left == null) {
+            fillTree(root.left, in);
+        } else {
+            fillTree(root.right, in);
         }
     }
 }
-
-
